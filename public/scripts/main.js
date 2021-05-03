700d8e44de27932a54e010fae0ceccacb3f9dadf
Max <paxipaxi2003@gmail.com>
2021-05-03 19:15:48 -0400

    3er deploy heroku

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,32 +1,3 @@
-const popup = document.querySelectorAll('#popup div')
-const products = document.querySelectorAll('.products div')
-
-
-let new_opacity = null
-
 const product1 = document.querySelector('.product1')
 
 product1.style.display = 'none'
-
-function fade() { // funcion que cada 200ms le suma 0.05 a un numero hasta llegar a 1
-    let opacity = 0
-    const suma = () => {
-        if (opacity >= 1) {
-            clearInterval(aburto)
-        } else {
-            opacity += 0.05
-            console.log(String(opacity))
-
-        }
-    }
-    var aburto = setInterval(suma, 200)
-}
-
-products.forEach((actual, i) => actual.addEventListener("click", () => {
-    popup[i].style.visibility = 'hidden'
-    fade()
-        // popup[i].style.opacity = new_opacity
-}))
-
-
-// popup[i].style.opacity
