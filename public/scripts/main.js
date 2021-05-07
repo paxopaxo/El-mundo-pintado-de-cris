a25c454b09a76963ab424d3cfd09afa0cb3b0b86
Max <paxipaxi2003@gmail.com>
2021-05-07 01:19:43 -0400

    Mejora interactividad

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,47 @@
 const product1 = document.querySelector('.product1')
+const header = document.querySelector('.header')
 
-product1.style.display = 'none'
+const sobreMi = document.querySelector('.header h2')
+
+// product1.style.display = 'none'
+
+function disableScroll(bool) {
+    if (bool) {
+        document.querySelector('body').classList.add('remove-scrolling')
+    } else {
+        document.querySelector('body').classList.remove('remove-scrolling')
+    }
+}
+
+if (screen.width <= 768) {
+    const responsiveMenu = document.querySelector('.responsive-menu')
+    const divClose = document.querySelector('.close')
+
+    sobreMi.addEventListener('touchend', () => {
+
+        const { left } = responsiveMenu.style
+
+        if (!left) {
+            responsiveMenu.style.left = '0'
+            disableScroll(true)
+        } else {
+            responsiveMenu.style.left = ''
+            disableScroll(false)
+        }
+    })
+
+    divClose.addEventListener('touchend', () => {
+        responsiveMenu.style.left = ''
+        disableScroll(false)
+    })
+
+} else {
+
+    setInterval(() => {
+        if (window.scrollY > 85) {
+            header.style.height = '125px'
+        } else {
+            header.style.height = ''
+        }
+    }, 200)
+}
